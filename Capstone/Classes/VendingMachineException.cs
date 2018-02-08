20bb66ac50aea145e72b4559e8efa04f6d6d6179
Craig Lawrie <craig.i.lawrie@gmail.com>
2018-02-08 15:45:04 -0500

    exceptions and part of purchase method

---
+++ Capstone/Classes/VendingMachineException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Capstone.Classes
+{
+    [Serializable]
+    public class VendingMachineException : Exception
+    {
+        public VendingMachineException()
+        {
+        }
+
+        public VendingMachineException(string message) : base(message)
+        {
+        }
+
+        public VendingMachineException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected VendingMachineException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
