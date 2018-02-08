5bebfd2b2b9ffe71f99e0103fe68ed700f1349f2
Kathy Walker <kjata1013@gmail.com>
2018-02-08 10:56:26 -0500

    set up initial tests

---
+++ CapstoneTests/ItemTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CapstoneTests
+{
+	[TestClass]
+	public class ItemTests
+	{
+		[TestMethod]
+		public void ProductExists()
+		{
+
+
+		}
+
+		[TestMethod]
+		public void CorrectItemDisplayed()
+		{
+
+		}
+
+		[TestMethod]
+		public void CorrectPriceDisplayed()
+		{
+
+		}
+	}
+}
---
+++ CapstoneTests/VendingTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Classes;
+
+namespace CapstoneTests
+{
+	[TestClass]
+	public class VendingTests
+	{
+		[TestMethod]
+		public void InventoryUpdated()
+		{
+		}
+
+		[TestMethod]
+		public void CorrectConsumeMessage()
+		{
+		}
+
+	}
+}
